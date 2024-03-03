73d88ac12bfcc655af1fa980e29fb67f1eda2084
devsantana <codejhonatansantana@gmail.com>
2024-03-03 14:52:55 -0300

    arquitetura feita

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:employee_app/onboarding/onboarding_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vagas de Emprego',
+      home: OnBoardingPage(),
+    );
+  }
+}
